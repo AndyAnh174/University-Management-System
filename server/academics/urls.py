c4734779f2e22462cf295c86f68bcd025242aa91
Hồ Việt Anh <hovietanh147@gmail.com>
2026-01-08 13:03:28 +0700

    feat: Add CRUD API for Major management with filtering and dropdown support

--- server/academics/urls.py
+++ server/academics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FacultyViewSet
+from .views import FacultyViewSet, MajorViewSet
 
 router = DefaultRouter()
 router.register(r'faculties', FacultyViewSet, basename='faculty')
+router.register(r'majors', MajorViewSet, basename='major')
 
 urlpatterns = [
     path('', include(router.urls)),
